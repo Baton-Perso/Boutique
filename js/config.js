800c75c042a71d8183c121c8dcd174e5cda27920
Baton-Perso <bap10official@gmail.com>
2026-02-16 16:14:10 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Configuration Supabase
+const SB_URL = "https://rryfiyupsmdqranvuqmx.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyeWZpeXVwc21kcXJhbnZ1cW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNDU5MTcsImV4cCI6MjA4NjgyMTkxN30.JzzAt3ejHhg66unhyw9Fc_uvlvtgJ4h7Pss5TUupD3g"; // Ta clé complète ici
+
+const supabase = supabase.createClient(SB_URL, SB_KEY);
